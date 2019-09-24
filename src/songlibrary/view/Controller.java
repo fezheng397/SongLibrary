b374acc76226beb83acff80641bcbf0c459af84a
fezheng397 <fezheng397@gmail.com>
2019-09-24 18:12:29 -0400

    Added imports to controller

---
+++ src/songlibrary/view/Controller.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package songlibrary.view;
+
+import javafx.event.Event;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import songlibrary.Song;
+
+/**
+ * @author Marco Vlajnic
+ *
+ */
+public class Controller {
+
+}
